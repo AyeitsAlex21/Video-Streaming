9882fabd25823b6ed22f05bca53c9026ae3d3153
Alex Summers <asummer8uoregon.edu>
2024-04-08 01:09:18 -0700

    initialized react and django

--- streamingApp/views.py
+++ streamingApp/views.py
@@ -3,10 +3,5 @@ from django.http import JsonResponse
 
 # Create your views here
 
-def index(request):
-    filepath = finders.find('my_app/index.html')
-    if filepath:
-        with open(filepath, 'r') as file:
-            return HttpResponse(file.read(), content_type="text/html")
-    else:
-        return HttpResponse("index.html not found", status=404)
+def list_videos(request):
+    return None
